370057311f102c0a530d2b2198a07298c9ac161e
dav_pr <dav_pr@tutanota.com>
2023-02-17 18:06:30 +0200

    setting db psql

--- candyshop/candyshop/settings.py
+++ candyshop/candyshop/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "candyshop.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'candyshopproject',
+        'USER': 'candyshopuser',
+        'PASSWORD': '283dav',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
